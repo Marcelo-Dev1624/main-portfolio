3fae8e4796d59bf6a64fd8621fc4203f01783881
MarceloVA <100235694+MarceloVA@users.noreply.github.com>
2023-01-08 16:21:07 -0600

    update

--- js/navbar.js
+++ js/navbar.js
@@ -1,5 +1,5 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const navbarLinks = document.getElementsByClassName('navbar-menu')[0]
 
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
