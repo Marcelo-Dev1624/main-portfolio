dec4fbea6904228d7f520b39dc7f947ea16d5c67
MarceloVA <100235694+MarceloVA@users.noreply.github.com>
2023-01-08 22:17:42 -0600

    update

---
+++ js/navbar-links-active-toggle.js
@@ -0,0 +1,11 @@
+let navbar = document.getElementsByClassName('navbar');
+let navbarLinks = navbar.getElementsByClassName('menu-item');
+
+for (var i =0; i<navbarLinks.length; i++)
+{
+    navbarLinks[i].addEventListener('click', function() {
+        var current = document.getElementsByClassName('active');
+        current[0].className = current[0].className.replace("active")
+        this.className += "active";
+    })
+}
