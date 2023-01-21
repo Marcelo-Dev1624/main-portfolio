66b5f48e7eb36a909a4ede2cddae61772aeee41a
MarceloVA <100235694+MarceloVA@users.noreply.github.com>
2023-01-20 22:25:08 -0600

    update

---
+++ js/active-link-on-scroll.js
@@ -0,0 +1,31 @@
+// Get all sections that have an ID defined
+const sections = document.querySelectorAll("section[id]");
+
+// Add an event listener listening for scroll
+window.addEventListener("scroll", navHighlighter);
+
+function navHighlighter() {
+  
+  // Get current scroll position
+  let scrollY = window.pageYOffset;
+  
+  // Now we loop through sections to get height, top and ID values for each
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+    
+    /*
+    - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navbar li link, else remove it
+    - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
+    */
+    if (
+      scrollY > sectionTop &&
+      scrollY <= sectionTop + sectionHeight
+    ){
+      document.querySelector(" .navbar-menu  li a[href*=" + sectionId + "]").classList.add("active");
+    } else {
+      document.querySelector(" .navbar-menu  li a [href*=" + sectionId + "]").classList.remove("active");
+    }
+  });
+}
